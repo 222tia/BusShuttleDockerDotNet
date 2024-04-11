9cd4af44059f9a1946a0648001ea8a4c7df37cb7
Tia Self <tiamarie.self@bsu.edu>
2024-04-11 10:57:21 -0400

    Create Login view

--- WebMvc/Controllers/HomeController.cs
+++ WebMvc/Controllers/HomeController.cs
@@ -31,6 +31,10 @@ public class HomeController : Controller
         this.userService = userService;
     }
 
+    public IActionResult ManagerView() {
+        return View();
+    }
+
     public IActionResult Index()
     {
         return View();
