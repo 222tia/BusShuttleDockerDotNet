3e25ae9859dc61333ad14e85675880cb8b2f4611
Tia Self <tiamarie.self@bsu.edu>
2024-04-20 13:43:17 -0400

    attempt to update entry view

--- WebMvc/Models/EntryViewModel.cs
+++ WebMvc/Models/EntryViewModel.cs
@@ -7,6 +7,10 @@ namespace WebMvc.Models;
 
 public class EntryViewModel {
     public int Id { get; set; }
+    public int StopId { get; set; }
+    public int LoopId { get; set; }
+    public int DriverId { get; set; }
+    public int BusId { get; set; }
     public DateTime TimeStamp { get; set; }
     public int Boarded { get; set; }
     public int LeftBehind { get; set; }
@@ -14,6 +18,10 @@ public class EntryViewModel {
     public static EntryViewModel FromEntry(EntryModel entry) {
         return new EntryViewModel {
             Id = entry.Id,
+            StopId = entry.StopId,
+            LoopId = entry.LoopId,
+            DriverId = entry.DriverId,
+            BusId = entry.BusId,
             TimeStamp = entry.TimeStamp,
             Boarded = entry.Boarded,
             LeftBehind = entry.LeftBehind 
